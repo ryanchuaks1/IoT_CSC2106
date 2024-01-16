b729f68ef6df546aa6971f4f160ffbed49eae0f4
Ryan Chua <ryanchuaks2000@gmail.com>
2024-01-16 09:55:54 +0800

    YOLOv8 Setup with region counter

--- raspberrypi4/main.py
+++ raspberrypi4/main.py
@@ -1,1 +1,29 @@
-print("hello world")
+import subprocess
+import json
+from typing import Dict
+
+
+def install_dependencies():
+    requirements_file = "requirements.txt"
+    subprocess.run(["pip", "install", "-r", requirements_file])
+
+
+def startYOLOModel(source) -> str:
+    subprocess.run(["python", "yolo.py", "--weights", "yolov8l.pt",
+                   "--source", source, "--view-img",])
+    vehicle_count = {
+
+    }
+
+    vehicle_count_json = json.dumps(vehicle_count)
+
+    return vehicle_count_json
+
+
+def main():
+    # install_dependencies()
+    startYOLOModel("traffic.mp4")
+
+
+if __name__ == "__main__":
+    main()
