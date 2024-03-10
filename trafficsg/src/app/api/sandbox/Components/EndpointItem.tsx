abcbf04b034e0df6482ea7752cea43a12565c08b
Sean Chan <seanchan@outlook.sg>
2024-03-10 16:41:22 +0800

    Created `components` and `types` for API sandbox

---
+++ trafficsg/src/app/api/sandbox/Components/EndpointItem.tsx
@@ -0,0 +1,16 @@
+import { MethodTag } from "./MethodTag";
+import { EndpointItemProps } from "../types";
+
+export const EndpointItem: React.FC<EndpointItemProps> = ({
+  method,
+  path,
+  description,
+}) => {
+  return (
+    <div className="border-b border-gray-200 p-4 flex items-center justify-start space-x-4 bg-white">
+      <MethodTag method={method} />
+      <span className="font-mono text-sm">{path}</span>
+      <span className="ml-auto text-sm">{description}</span>
+    </div>
+  );
+};
---
+++ trafficsg/src/app/api/sandbox/types.ts
@@ -0,0 +1,11 @@
+export interface MethodTagProps {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+}
+
+export interface Endpoint {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  path: string;
+  description: string;
+}
+
+export interface EndpointItemProps extends Endpoint {}
