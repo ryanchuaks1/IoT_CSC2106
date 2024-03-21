d052aa56bd202f7cf5b558652865d69929adaced
Sean Chan <seanchan@outlook.sg>
2024-03-21 15:44:32 +0800

    Update `<Header />` component

--- trafficsg/src/app/component/header.tsx
+++ trafficsg/src/app/component/header.tsx
@@ -2,16 +2,28 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="w-screen flex justify-between border-b-slate-200 border-b-2 p-4 shadow-sm sticky top-0">
-      <div className="text-3xl font-semibold font-['Rockwell'] place-self-center">
-        Traffic <span className="text-primary">SG</span>
-      </div>
+    <div className="w-screen flex justify-between border-b-slate-100 border-b-2 p-4 shadow-sm sticky top-0 bg-white">
       <Link
-        href={"/api/sandbox"}
-        className="p-2 border-2 rounded-md hover:bg-slate-100"
+        href={"/"}
+        className="text-3xl font-semibold font-['Rockwell'] place-self-center"
       >
-        API Sandbox
+        Traffic <span className="text-primary">SG</span>
       </Link>
+      
+      <nav className="flex">
+        <Link
+          href={"/traffic"}
+          className="p-2 mr-2 border-2 rounded-md hover:bg-slate-100"
+        >
+          Traffic
+        </Link>
+        <Link
+          href={"/api/sandbox"}
+          className="p-2 border-2 rounded-md hover:bg-slate-100"
+        >
+          API Sandbox
+        </Link>
+      </nav>
     </div>
   );
 }
